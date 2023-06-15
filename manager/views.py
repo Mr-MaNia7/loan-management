bff7266225f8178ab080706103b0e1d413e714af
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2023-06-15 14:16:47 +0300

    User View

--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import User
 
 
 def showHomeView(request):
     return render(request, "index.html")
+
+class UserListView(ListView):
+    model = User
+    template_name = 'user_list.html'
+    context_object_name = 'users'
+
+class UserCreateView(CreateView):
+    model = User
+    template_name = 'user_create.html'
+    fields = ['username', 'email', 'first_name', 'last_name']
+    success_url = reverse_lazy('user_list')
+
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = 'user_update.html'
+    fields = ['username', 'email', 'first_name', 'last_name']
+    success_url = reverse_lazy('user_list')
+
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = 'user_delete.html'
+    success_url = reverse_lazy('user_list')
