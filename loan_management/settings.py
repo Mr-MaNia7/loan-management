8e0361a140006b29f71a5515fdcdd20e37d845f8
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2023-06-19 23:05:17 +0300

    Add extensions to reset databse

--- loan_management/settings.py
+++ loan_management/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'manager',
-    'django_bootstrap5'
+    'django_bootstrap5',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
